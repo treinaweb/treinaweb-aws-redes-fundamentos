249506778485fd476ff53e28c261d0790f125dde
gmsantos <gabriel.ms1@hotmail.com>
2020-08-25 13:35:16 -0300

    Make request to API

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+@Injectable()
 export class AppComponent {
   title = 'spa';
+  response;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:3000/api').subscribe(
+      data => this.response = JSON.stringify(data),
+      error => this.response = error.message
+    )
+  }
 }
